502bd89e34fb1ed77b43a003e4cb99dc68b88fa9
Raja Beemi <r.beemireddy@global.com>
2020-02-07 09:28:54 +0000

    Read me added

---
+++ README.md
@@ -0,0 +1,10 @@
+## Extentreport test framework
+
+This ExtentReport test framework based on [extentreports](https://extentreports.com/)
+
+### Run tests
+    ```mvn clean test```
+This will generate the extent html report under test-outout folder
+
+### Contact
+[beemi.raja@gmail.com](beemi.raja@gmail.com)
---
+++ src/test/java/com/agikya/digital/RestTest.java
@@ -0,0 +1,31 @@
+package com.agikya.digital;
+
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners({ExtentITestListenerClassAdapter.class})
+public class RestTest {
+
+    @Test
+    public void passTest() {
+
+        Assert.assertEquals(5,3+2);
+    }
+
+    @Test
+    public void failTest() {
+
+
+        Assert.assertNotEquals(2,3+2);
+    }
+
+    @Test
+    @Ignore
+    public void skipTest() {
+
+        Assert.assertEquals(2,3+2);
+    }
+}
