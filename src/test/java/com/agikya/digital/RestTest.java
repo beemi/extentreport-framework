40ede771eb1eb0715f8ced4c0c29587eee8d45cb
Raja Beemi <r.beemireddy@global.com>
2020-02-08 12:02:53 +0000

    Rest api test added

--- src/test/java/com/agikya/digital/RestTest.java
+++ src/test/java/com/agikya/digital/RestTest.java
@@ -1,7 +1,12 @@
 package com.agikya.digital;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
+import io.restassured.RestAssured;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -9,23 +14,36 @@ import org.testng.annotations.Test;
 @Listeners({ExtentITestListenerClassAdapter.class})
 public class RestTest {
 
-    @Test(groups = {"t:DOVE-500", "a:Raja Beemi"})
-    public void passTest() {
+    ExtentReports extentReports;
 
-        Assert.assertEquals(5,3+2);
+    @BeforeTest
+    public void setUp() {
+
+        extentReports = new ExtentReports();
     }
 
-    @Test(groups = {"t:DOVE-501", "a:Raja Beemi"})
+
+    @Test(groups = {"t:JIRA-500", "a:Raja Beemi"})
+    public void postcodeTest() {
+
+        var response = RestAssured.given()
+                                  .get("http://api.postcodes.io/postcodes/RM17 6EY")
+                                  .andReturn();
+
+        response.then().log().all().statusCode(200);
+    }
+
+    @Test(groups = {"t:JIRA-501", "a:Raja Beemi"})
     public void failTest() {
 
 
-        Assert.assertNotEquals(2,3+2);
+        Assert.assertNotEquals(2, 3 + 2);
     }
 
     @Test
     @Ignore
     public void skipTest() {
 
-        Assert.assertEquals(2,3+2);
+        Assert.assertEquals(2, 3 + 2);
     }
 }
